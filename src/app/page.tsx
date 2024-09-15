c1308974c027fe7a44f65cc0f3798f315aa56428
Gernii <anhducngole2001@gmail.com>
2024-09-15 21:46:24 +0700

    fix lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import { ChatBubble } from "@/components/chat-bubble";
-import Image from "next/image";
 
 export default function Home() {
     return (
         <div className="h-[100dvh] max-w-4xl mx-auto px-4 flex flex-col">
             <div className="flex-grow">
-                <ChatBubble
-                    message="Hello, how can i help you"
-                    possition="left"
-                />
+                <ChatBubble message="Hello, how can i help you" possition="left" />
                 <ChatBubble message="I need your help" possition="right" />
             </div>
             <form className="w-full py-4 flex-shrink-0 flex gap-x-2 sticky bottom-0 inset-0 bg-slate-950">
