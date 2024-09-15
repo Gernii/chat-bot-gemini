720434ebf71ccc925100fd05498ded6d585e3796
Gernii <anhducngole2001@gmail.com>
2024-09-15 21:45:16 +0700

    feat(web): implement wireframe ui

---
+++ src/components/chat-bubble/chat-bubble.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { cx } from "cva";
+
+export interface ChatBubbleProps {
+    message: string;
+    possition: "left" | "right";
+}
+
+export const ChatBubble: FC<ChatBubbleProps> = ({ message, possition }) => {
+    const containerClassName = cx("my-4 flex", {
+        "justify-start": possition === "left",
+        "justify-end": possition === "right",
+    });
+
+    const bubbleClassName = cx("p-4 rounded-xl", {
+        "bg-slate-600": possition === "left",
+        "bg-slate-700": possition === "right",
+    });
+    return (
+        <div className={containerClassName}>
+            <div className={bubbleClassName}>{message}</div>
+        </div>
+    );
+};
